f3c14becc5eed1f88d9b73394f79b9f674924691
Ramzi Al-Dorikhim <ramzik149@gmail.com>
2022-05-02 17:48:46 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Запускаем игру");
+        JFrame window = new JFrame("TicTacToe"); // главное меню
+        window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);// закрытие окна
+        window.setSize(400, 400); // размер окна
+        window.setLayout(new BorderLayout()); //менеджер компановки
+        window.setLocationRelativeTo(null); // разместить окно в центре
+        window.setVisible(true); //видимость окна
+        TicTacToe game = new TicTacToe();
+        window.add(game);
+        System.out.println("Игра оконченааааааааааааааааа");
+    }
+}
---
+++ src/com/company/TicTacToe.java
@@ -0,0 +1,170 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+
+public class TicTacToe extends JComponent {
+    public static final int FIELD_EMPTY = 0; //пустое поле
+    public static final int FIELD_X = 10; // поле с крестиком
+    public static final int FIELD_O = 200; // поле с ноликом
+    int[][] field; // массив игрового поля
+    boolean isXturn; // true - ход X , false - ход O
+
+    public TicTacToe(){
+        enableEvents(AWTEvent.MOUSE_EVENT_MASK);
+        field = new int[3][3]; // выделяем память
+        initGame();
+    }
+
+    public void initGame() {
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 3; ++j) {
+                field[i][j] = FIELD_EMPTY;
+            }
+        }
+        isXturn = true;
+    }
+
+    @Override
+    protected void processMouseEvent(MouseEvent mouseEvent) {
+        super.processMouseEvent(mouseEvent);
+        if (mouseEvent.getButton() == MouseEvent.BUTTON1) {
+            int x = mouseEvent.getX(); // координата x клика
+            int y = mouseEvent.getY(); // координата y клика
+            // переводим координаты в индексы ячейки в массиве field
+            int i = (int) ((float) x / getWidth() * 3);
+            int j = (int) ((float) y / getHeight() * 3);
+            // проверяем, что выбранная ячейка пусьа и туда можно сходить
+            if (field[i][j] == FIELD_EMPTY) {
+                // чей ход
+                field[i][j] = isXturn ? FIELD_X : FIELD_O;
+                isXturn = !isXturn; // меняем флаг хода
+                repaint(); // перерисовка компонента
+            }
+            int res = checkState();
+            if(res != 0){
+                if(res == FIELD_O * 3){
+                    //победил O
+                    JOptionPane.showMessageDialog(this, "Нолики выйграли",
+                            "Победа!", JOptionPane.INFORMATION_MESSAGE);
+                } else if(res == FIELD_X * 3){
+                    // победилп x
+                    JOptionPane.showMessageDialog(this, "Крестики выйграли",
+                            "Победа!", JOptionPane.INFORMATION_MESSAGE);
+                } else {
+                    JOptionPane.showMessageDialog(this, "Никто не выйграл!",
+                            "Ничья!", JOptionPane.INFORMATION_MESSAGE);
+                }
+                initGame();
+                repaint();
+            }
+
+        }
+
+    }
+
+
+
+    @Override
+    protected void paintComponent(Graphics graphics ){
+        super.paintComponent(graphics);
+        //очищаем область
+        graphics.clearRect(0, 0, getWidth(), getHeight());
+        // рисуем сетку
+        drawGrid(graphics);
+        // рисуем текущие крестики и нолики
+        drawXO(graphics);
+    }
+
+    void drawGrid(Graphics graphics){
+        int w = getWidth(); //ширина игрового поля
+        int h = getHeight(); // высота игрового поля
+        int dw = w / 3; //делим ширину на 3
+        int dh = h / 3; //делим высоту на 3
+        graphics.setColor(Color.BLUE); //цвет линий
+        for (int i = 1; i < 3; i++) {
+            graphics.drawLine(0, dh * i, w, dh * i);
+            graphics.drawLine(dw * i, 0, dw * i , h);
+        }
+    }
+
+
+    void drawX(int i, int j, Graphics graphics){
+        graphics.setColor(Color.BLACK);
+        int dw = getWidth() / 3;
+        int dh = getHeight() / 3;
+        int x = i * dw;
+        int y = j * dh;
+        //линия от верхнего левого угла в правый нижний
+        graphics.drawLine(x, y, x + dw, y + dh);
+        //линия от левого нижнего угла до правого верхнего
+        graphics.drawLine(x, y + dh, x + dw, y);
+    }
+
+    void draw0(int i, int j, Graphics graphics){
+        graphics.setColor(Color.BLACK);
+        int dw = getWidth() / 3;
+        int dh = getHeight() / 3;
+        int x = i * dw;
+        int y = j * dh;
+        // вытягиваем окружность
+        graphics.drawOval(x, y, dw, dh);
+    }
+
+    void drawXO(Graphics graphics){
+        //вложенные циклы
+        for (int i = 0; i < 3; ++i){
+            for (int j = 0; j < 3; ++j){
+                //если в данной ячейке крестик - рисуем его
+                if (field[i][j] == FIELD_X){
+                    drawX(i, j, graphics);
+                } else if (field[i][j] == FIELD_O){
+                    draw0(i, j, graphics);
+                }
+            }
+        }
+    }
+
+    int checkState() {
+        // проверка диагоналей
+        int diag = 0;
+        int diag2 = 0;
+        for (int i = 0; i < 3; i++) {
+            //сумма значений по диагонали от левого угла
+            diag += field[i][i];
+            // сумма значений по диагонали от правого угла
+            diag2 += field[i][2 - i];
+        }
+        if(diag == FIELD_O * 3 || diag == FIELD_X * 3)
+            return diag;
+        if(diag2 == FIELD_O * 3 || diag2 == FIELD_X * 3)
+            return diag2;
+        int check_i, check_j;
+        boolean hasEmpty = false;
+        //будем бешать по всем рядам
+        for(int i = 0 ; i < 3; i++){
+            check_i = 0;
+            check_j = 0;
+            for(int j=0; j < 3; j++){
+                //суммируем знаки в текущем ряду
+                if(field[i][j] == 0){
+                    hasEmpty = true;
+                }
+                check_i += field[i][j];
+                check_j += field[j][i];
+            }
+            // если выигрыш крестика или нолика, то выхоим
+            if(check_i == FIELD_O*3 || check_i == FIELD_X * 3){
+                return check_i;
+            }
+            if(check_j == FIELD_O * 3 || check_j == FIELD_X * 3){
+                return check_j;
+            }
+        }
+
+        if(hasEmpty) return 0;
+        else return -1;
+    }
+}
+
